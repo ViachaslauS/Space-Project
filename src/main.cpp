42b573c2d7ed9bd4b112a02fe44b8fe7b5adafc9
alex-eg <alexey.e.egorov@gmail.com>
2025-01-03 17:30:38 +0300

    Fix indents and window closing

--- src/main.cpp
+++ src/main.cpp
@@ -11,64 +11,64 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "GameObject.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "resource_dir.h" // utility header for SearchAndSetResourceDir
 
 #define RAYGUI_IMPLEMENTATION
 #include "external/raygui.h"
 
 int main ()
 {
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+    // Tell the window to use vsync and work on high DPI displays
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+    // Create the window and OpenGL context
+    InitWindow(1280, 800, "Hello Raylib");
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+    // Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+    SearchAndSetResourceDir("resources");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+    // Load a texture from the resources directory
+    Texture wabbit = LoadTexture("wabbit_alpha.png");
 
-	bool showMessageBox = false;
-	bool quitGame = false;
+    bool showMessageBox = false;
+    bool quitGame = false;
 
-	VitalityParams tempParams;
-	GameObject tempObj(tempParams);
+    VitalityParams tempParams;
+    GameObject tempObj(tempParams);
 
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
-	{
-		const float dt = GetFrameTime();
-		tempObj.update(dt);
+    // game loop
+    while (!WindowShouldClose()) // run the loop untill the user presses ESCAPE or presses the Close button on the window
+    {
+        const float dt = GetFrameTime();
+        tempObj.update(dt);
 
-	 BeginDrawing();
-            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+        BeginDrawing();
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-			tempObj.render();
+        tempObj.render();
 
-			if (GuiButton((Rectangle { 600, 370, 120, 30 }), "Start Game")) showMessageBox = true;
+        if (GuiButton((Rectangle { 600, 370, 120, 30 }), "Start Game")) showMessageBox = true;
 
-			if (GuiButton((Rectangle{ 600, 450, 120, 30 }), "Rage Quit")) quitGame = true;
+        if (GuiButton((Rectangle{ 600, 450, 120, 30 }), "Rage Quit")) quitGame = true;
 
-            if (showMessageBox)
-            {
-               //Start Game logic
-            }
-
-			if (quitGame)
-			{
-				CloseWindow();
-			}
+        if (showMessageBox)
+        {
+            //Start Game logic
+        }
 
         EndDrawing();
-	}
 
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+        if (quitGame)
+        {
+            break;
+        }
+    }
 
-	// destroy the window and cleanup the OpenGL context
-	CloseWindow();
-	return 0;
+    // cleanup
+    // unload our texture so it can be cleaned up
+    UnloadTexture(wabbit);
+
+    // destroy the window and cleanup the OpenGL context
+    CloseWindow();
+    return 0;
 }
