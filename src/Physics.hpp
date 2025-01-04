908e8249a03088215f17bd68178f37a36b2328b7
alex-eg <alexey.e.egorov@gmail.com>
2025-01-05 00:53:08 +0300

    Add Physics to GameplayManager

--- src/Physics.hpp
+++ src/Physics.hpp
@@ -5,13 +5,13 @@
 
 #include "box2d/box2d.h"
 
-#include "GameObject.h"
-
 struct Vector2;
+class GameObject;
 
 struct PhysicsComp
 {
-    ObjectType type;
+    GameObject *object;
+    b2ShapeId shapeId;
     b2BodyId id;
 };
 
@@ -20,15 +20,15 @@ struct Physics
     Physics();
     ~Physics();
 
-    PhysicsComp createRectangularBody(const Vector2 &center, float width, float height, GameObject *object);
-    PhysicsComp createCircularBody(const Vector2 &center, float radius, GameObject *object);
+    PhysicsComp* createRectangularBody(const Vector2 &center, float width, float height, GameObject *object);
+    PhysicsComp* createCircularBody(const Vector2 &center, float radius, GameObject *object);
 
-    bool removeBody(const PhysicsComp &comp);
+    bool removeBody(PhysicsComp *comp);
     void update();
     void debugRender();
     void toggleDebugRender();
 
-    std::vector<PhysicsComp> comps;
+    std::vector<std::unique_ptr<PhysicsComp>> comps;
 
     bool debugDraw = false;
 
